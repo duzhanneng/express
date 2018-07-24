575e982eaa9cd88a51bae6209338a596c50cf5e2
duzhanneng <duzhanneng@bigo.sg>
2018-07-24 15:57:47 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // node 后端服务器入口
 var http = require('http');
-// var userApi = require('./api/userApi');
+var userApi = require('./api/userApi');
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser'); // node.js 中间件，用于处理 JSON, Raw, Text 和 URL 编码的数据
@@ -8,10 +8,17 @@ var cookieParser = require('cookie-parser') // 解析Cookie的工具。通过req
 var express = require('express');
 var app = express();
 
+app.engine( '.html', require( 'ejs' ).__express );
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
-app.use('/dzn', express.static(path.join(__dirname, './index.html')))
+// app.use('/dzn', express.static(path.join(__dirname, './index.html')))
+app.use(express.static(path.join(__dirname, './dist')))
+
+app.get('/', function (req, res) {
+  res.render('./dist');
+});
 
 //全局处理
 app.use(function(req, res, next) {
@@ -27,7 +34,7 @@ app.use(function(req, res, next) {
 });
 
 //注册api路由
-// app.use('/api/userController', userApi);
+app.use('/api/userController', userApi);
 
 // 监听端口
 app.listen(8082);
